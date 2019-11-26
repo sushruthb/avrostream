a771dffd05e0c61d46905c414e4fb6a4330ea9c6
Sushruth Bharath HG <bharathhg@gmail.com>
2019-11-26 16:40:45 +0530

    First commit

---
+++ build.sbt
@@ -0,0 +1,25 @@
+
+name := "avrostreaming"
+
+version := "0.1"
+
+scalaVersion := "2.12.0"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-sql" % "2.4.4",
+
+  "org.apache.spark" %% "spark-avro" % "2.4.4"
+
+)
+
+resolvers += "confluent" at "http://packages.confluent.io/maven/"
+resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+resolvers += "MavenCentral" at "https://mvnrepository.com/"
+resolvers += Resolver.url("bintray-sbt-plugins", url("https://dl.bintray.com/eed3si9n/sbt-plugins/"))(Resolver.ivyStylePatterns)
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  //To add Kafka as source
+  case "META-INF/services/org.apache.spark.sql.sources.DataSourceRegister" => MergeStrategy.concat
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/com/stream/avro/ReadAvro.scala
@@ -0,0 +1,34 @@
+package com.stream.avro
+
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object ReadAvro {
+
+  def main(args:Array[String]): Unit ={
+
+    val spark= SparkSession.builder()
+      .appName("Spark-Avro")
+      .getOrCreate()
+
+    val data = Seq(("James ", "", "Smith", 2018, 1, "M", 3000),
+      ("Michael ", "Rose", "", 2010, 3, "M", 4000),
+      ("Robert ", "", "Williams", 2010, 3, "M", 4000),
+      ("Maria ", "Anne", "Jones", 2005, 5, "F", 4000),
+      ("Jen", "Mary", "Brown", 2010, 7, "", -1)
+    )
+
+    val columns = Seq("firstname", "middlename", "lastname", "dob_year",
+      "dob_month", "gender", "salary")
+
+    import spark.sqlContext.implicits._
+    val df = data.toDF(columns: _*)
+
+    df.write.format("avro")
+      .mode(SaveMode.Overwrite)
+      .save("/home/dev/person.avro")
+    spark.read.format("avro").load("/home/dev/person.avro").show()
+
+  }
+
+}
